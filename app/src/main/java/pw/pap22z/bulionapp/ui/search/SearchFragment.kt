352223d4615e2d02fbd67ff6904432c93489f4f8
jkowalc5 <01169190@pw.edu.pl>
2023-01-04 15:34:50 +0100

    Room Database + SearchView (problem with onMenuItemSelected)

--- app/src/main/java/pw/pap22z/bulionapp/ui/search/SearchFragment.kt
+++ app/src/main/java/pw/pap22z/bulionapp/ui/search/SearchFragment.kt
@@ -2,7 +2,7 @@ package pw.pap22z.bulionapp.ui.search
 
 import android.os.Bundle
 import android.view.*
-import androidx.appcompat.widget.SearchView
+import android.widget.Toast
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
@@ -12,18 +12,14 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import pw.pap22z.bulionapp.R
 import pw.pap22z.bulionapp.databinding.FragmentSearchBinding
 
-//@AndroidEntryPoint
 class SearchFragment : Fragment(R.layout.fragment_search), MenuProvider {
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
 
     //private val searchViewModel: SearchViewModel by viewModels()
     lateinit var viewModel: SearchViewModel
-    //private val sAdapter: SearchAdapter by lazy { SearchAdapter() }
-
+    private val sAdapter: SearchAdapter by lazy { SearchAdapter() }
     //private lateinit var recyclerView: RecyclerView
 
 
@@ -32,30 +28,17 @@ class SearchFragment : Fragment(R.layout.fragment_search), MenuProvider {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        //val searchViewModel =
-            //ViewModelProvider(this).get(SearchViewModel::class.java)
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
-        //val root = inflater.inflate(R.layout.fragment_search, container, false)
-//        val menuHost: MenuHost = requireActivity()
-//        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
-        //binding.recyclerView.layoutManager = LinearLayoutManager(context)
-        //binding.recyclerView.adapter = sAdapter
-//        recyclerView.setHasFixedSize(true)
-        //searchViewModel.insertData(Restaurant("Aioli", "Włoska"))
 //        searchViewModel.readData.observe(viewLifecycleOwner) {
 //            sAdapter.setData(it)
 //        }
         return binding.root
     }
 
-    //    _binding = FragmentSearchBinding.inflate(inflater, container, false)
-//        binding.recyclerView.layoutManager = LinearLayoutManager(context)
-//        binding.recyclerView.adapter = SearchAdapter
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentSearchBinding.bind(view)
-        val sAdapter = SearchAdapter()
+        //val sAdapter = SearchAdapter()
         binding.apply {
             recyclerView.apply {
                 adapter = sAdapter
@@ -67,82 +50,62 @@ class SearchFragment : Fragment(R.layout.fragment_search), MenuProvider {
             this,
             ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
         ).get(SearchViewModel::class.java)
-        //searchViewModel.insertData(Restaurant("Aioli", "Włoska"))
+//        viewModel.insertData(Restaurant("si ristorante", "test2"))
+//        viewModel.insertData(Restaurant("aioli", "test1"))
         //sAdapter.setData(listOf(Restaurant("aioli", "test1"), Restaurant("lapose", "test2")))
-//        searchViewModel.readData.observe(viewLifecycleOwner) {
-//            sAdapter.setData(it)
-//        }
+        viewModel.allRestaurants.observe(viewLifecycleOwner) { list ->
+            list?.let {
+                sAdapter.setData(it)
+            }
+        }
         //search
         val menuHost: MenuHost = requireActivity()
         menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 
     override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
         menuInflater.inflate(R.menu.search_menu_item, menu)
     }
 
+    override fun onPrepareMenu(menu: Menu) {
+        Toast.makeText(requireContext(), "prepared", Toast.LENGTH_SHORT).show()
+        super.onPrepareMenu(menu)
+    }
+
     override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-        val searchView = menuItem.actionView as SearchView
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
-            override fun onQueryTextSubmit(query: String): Boolean {
-                return true
+        when(menuItem.itemId) {
+            R.id.search_action -> {
+                Toast.makeText(requireContext(), "item selected", Toast.LENGTH_SHORT).show()
+                return false
             }
-            override fun onQueryTextChange(query: String): Boolean {
-//                if(query != null){
-//                    val searchQuery = "%$query%"
-//
-//                    searchViewModel.searchDatabase(searchQuery).observe(viewLifecycleOwner) { list ->
-//                        list.let {
-//                            adapter.setData(it)
-//                        }
-//                    }
-//                }
-                return true
             }
-        })
+        //val searchView = menuItem.actionView as SearchView
+        //Toast.makeText(requireContext(), "item selected", Toast.LENGTH_SHORT).show()
+//        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+//            override fun onQueryTextSubmit(query: String): Boolean {
+//                Toast.makeText(requireContext(), "submit", Toast.LENGTH_SHORT).show()
+//                return false
+//            }
+//            override fun onQueryTextChange(query: String): Boolean {
+//                Toast.makeText(requireContext(), "text changed", Toast.LENGTH_SHORT).show()
+////                if(query.isNotEmpty()){
+////                    val searchQuery = "%$query%"
+////
+////                    viewModel.searchDatabase(searchQuery).observe(viewLifecycleOwner) { list ->
+////                        list.let {
+////                            sAdapter.setData(it)
+////                        }
+////                    }
+////                }
+//                return false
+//            }
+//        })
         return true
     }
 
-//    private fun filter(text: String){
-//        val filteredlist: ArrayList<RestaurantSearch> = ArrayList()
-//        for (item in restaurantsArrayList){
-//            if (item.description.lowercase().contains(text.lowercase())){
-//                filteredlist.add(item)
-//            }
-//        }
-//        if (filteredlist.isEmpty()){
-//            Toast.makeText(activity, "No Data Found..", Toast.LENGTH_SHORT).show()
-//        } else {
-//            adapter.filterList((filteredlist))
-//        }
-//    }
-//
-//
-//    private fun dataInitialize(){
-//        imageId = arrayListOf(
-//            R.drawable.a,
-//            R.drawable.b,
-//            R.drawable.c,
-//            R.drawable.d,
-//            R.drawable.e
-//        )
-//        description = arrayOf(
-//            getString(R.string.description_1),
-//            getString(R.string.description_2),
-//            getString(R.string.description_3),
-//            getString(R.string.description_4),
-//            getString(R.string.description_5),
-//
-//        )
-//        for(i in imageId.indices){
-//            val restaurant = RestaurantSearch(imageId[i], description[i])
-//            restaurantsArrayList.add(restaurant)
-//        }
-//    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
 
--- app/src/main/java/pw/pap22z/bulionapp/ui/search/SearchViewModel.kt
+++ app/src/main/java/pw/pap22z/bulionapp/ui/search/SearchViewModel.kt
@@ -2,9 +2,9 @@ package pw.pap22z.bulionapp.ui.search
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import pw.pap22z.bulionapp.data.Repository
 import pw.pap22z.bulionapp.data.Restaurant
@@ -12,20 +12,20 @@ import pw.pap22z.bulionapp.data.RestaurantDatabase
 
 
 class SearchViewModel (application: Application) : AndroidViewModel(application){
-    val allRestaurants : Flow<List<Restaurant>>
+    val allRestaurants : LiveData<List<Restaurant>>
     val repository: Repository
 
     init {
         val dao = RestaurantDatabase.getDatabase(application).restaurantDao()
         repository = Repository(dao)
-        allRestaurants = repository.readData()
+        allRestaurants = repository.allRestaurants
     }
 
     fun insertData(restaurant: Restaurant) =
         viewModelScope.launch(Dispatchers.IO) {
             repository.insertData(restaurant)
         }
-//    fun searchDatabase(searchQuery: String): LiveData<List<Restaurant>> {
-//        return repository.searchDatabase(searchQuery).asLiveData()
-//    }
+    fun searchDatabase(searchQuery: String): LiveData<List<Restaurant>> {
+        return repository.searchDatabase(searchQuery)
+    }
 }
